0ee32434fc6281e9336dab2d9560d0abe33ef957
HopeMashal <mashal.hope@gmail.com>
2022-08-20 23:23:30 +0300

    add model data efficiently

---
+++ Notes/ModelDataEfficiently.js
@@ -0,0 +1,37 @@
+//! Booleans 
+let isLoggedIn = true;
+let gameOver = false;
+const isWaterWet = true;
+
+//? Variable can change type Ex:
+
+let numDonuts = 12;
+numDonuts = false;
+numDonuts = 1234;
+
+//! Strings 
+let firstName = "Hope";
+let lastName = "Mashal";
+let MyName = firstName+" "+lastName; // Hope Mashal
+let msg = "Hi There!! My name is Hope/Amal";
+let name = 'Hope';
+let num = 87;
+
+typeof firstName; //String
+typeof num; //Number
+
+//! String Indices 
+
+//! String Methods 
+
+//! String Escape Characters 
+
+//! String Template Literals 
+
+//! Null & Undefined 
+
+//! Math Object & Random Numbers 
+
+//! typeof Operator
+
+//! parseInt & parseFloat 
--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 JavaScript Basic Notes and Some Apps
 
 # Notes
-* JS Values & Variables [GO]()
+* JS Values & Variables [GO](https://github.com/HopeMashal/Java-Script/blob/master/Notes/Values%2B%26%2BVariables.pdf)
+* Model Data Efficiently [GO](https://github.com/HopeMashal/Java-Script/blob/master/Notes/ModelDataEfficiently.js)
+  * Booleans 
+  * Strings 
+  * String Indices 
+  * String Methods 
+  * String Escape Characters 
+  * String Template Literals 
+  * Null & Undefined 
+  * Math Object & Random Numbers 
+  * typeof Operator
+  * parseInt & parseFloat 
 
 # Apps
 
